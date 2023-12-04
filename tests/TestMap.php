90b2921d4e0821b60357119c71d0b4cb16e0c57a
axdelafuen <axel.dlf@free.fr>
2023-12-04 13:46:55 +0000

    :white_check_mark: unit tests for map.php

--- tests/TestMap.php
+++ tests/TestMap.php
@@ -17,4 +17,56 @@ final class TestMap extends TestCase
 
         $this->assertSame($path, $map->getPath());
     }
+
+    /**
+     * @covers Map::set
+     * @covers Map::getElements
+     */
+    public function testSetData():void
+    {
+        $data = array();
+        $data[] = new Sign("test");
+
+        $map = new Map("path.path");
+
+        $map->set($data);
+
+        $this->assertSame($data[0], $map->getElements()[0]);
+    }
+
+    /**
+     * @covers Map::addElement
+     * @covers Map::getElements
+     * @covers Map::removeElement
+     */
+    public function testAddAndRemoveElement():void
+    {
+        $element = new Sign("test");
+
+        $map = new Map("path.path");
+
+        $map->addElement($element);
+
+        $this->assertSame($element, $map->getElements()[0]);
+
+        $map->removeElement($element);
+
+        $this->assertSame(0,count($map->getElements()));
+    }
+
+    /**
+     * @covers Map::getElementById
+     */
+    public function testGetElementById():void
+    {
+        $element = new Sign("test");
+
+        $map = new Map("path.path");
+
+        $map->addElement($element);
+
+        $elementById = $map->getElementById($element->getId());
+
+        $this->assertSame($elementById->getId(), $element->getId());
+    }
 }
