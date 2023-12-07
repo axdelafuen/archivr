b29fd40a01229c5dacd1c8c49e712399d7293af4
aurian.jault <aurian.jault@etu.uca.fr>
2023-12-07 10:59:28 +0100

    Fix raycasting problems

--- Panorama/ressources/javaScript/deviceHandler.js
+++ Panorama/ressources/javaScript/deviceHandler.js
@@ -3,10 +3,16 @@ AFRAME.registerComponent('scene', {
     if(AFRAME.utils.device.isMobile ())
     {
       mobileComponent(this.el)
+      this.el.addEventListener('enter-vr', function () {
+      addPanel(false);
+    });
     }
     else
     {
       computerComponent(this.el)
+      this.el.addEventListener('enter-vr', function () {
+      addPanel(true);
+    });
     }
   },
 });
@@ -23,6 +29,7 @@ function computerComponent(scene)
   panel.setAttribute("rotation","-50 0 0")
   panel.setAttribute("position","0 -0.8 -0.3")
   scene.append(panel) 
+  
 }
 
 function mobileComponent()
@@ -35,32 +42,6 @@ function mobileComponent()
   link.type ='text/css';
   link.href ='./ressources/css/style.css';
   document.getElementsByTagName('HEAD')[0].appendChild(link);
-  document.querySelector('a-scene').addEventListener('enter-vr', function () {
-    if(AFRAME.utils.device.checkHeadsetConnected ())
-    {
-      let panel = document.createElement("a-image")
-      panel.setAttribute("src","./ressources/assets/computerBinding.png")
-      panel.setAttribute("rotation","-50 0 0")
-      panel.setAttribute("position","0 -2 -1")
-      this.append(panel)
-      
-      let leftHand = document.createElement("a-entity")
-      let rightHand =  document.createElement("a-entity")
-
-      leftHand.setAttribute("id","left")
-      rightHand.setAttribute("id","right")
-
-      rightHand.setAttribute("hand-controls")
-      leftHand.setAttribute("hand-controls")
-
-      leftHand.setAttribute("laser-controls","hand:left")
-      rightHand.setAttribute("laser-controls","hand: right")
-
-      leftHand.setAttribute("raycaster","showLine: true;lineColor: blue; lineOpacity: 1")
-      rightHand.setAttribute("raycaster","showLine: true;lineColor: red; lineOpacity: 1")
-
-      scene.append(leftHand)
-      scene.append(rightHand)
-    }
-  });
 }
+
+
--- Panorama/ressources/javaScript/functions.js
+++ Panorama/ressources/javaScript/functions.js
@@ -34,4 +34,43 @@ async function goTo(file,rotation="0 0 0")
   
   let camera = document.querySelector("#camera")
   camera.setAttribute("rotation",rotation)
+  // Set slider's value to 0
+  yearsVector = 0
+}
+
+
+
+function addPanel(computer)
+{
+  if(AFRAME.utils.device.checkHeadsetConnected ())
+    {
+      let camera = document.querySelector("#player")
+
+      let panel = document.createElement("a-image")
+      if(!computer)
+      {
+        panel.setAttribute("src","./ressources/assets/computerBinding.png")
+        panel.setAttribute("rotation","-50 0 0")
+        panel.setAttribute("position","0 -2 -1")
+        document.querySelector("a-scene").append(panel)
+      }
+            
+      let leftHand = document.createElement("a-entity")
+      let rightHand =  document.createElement("a-entity")
+
+      leftHand.setAttribute("id","left")
+      rightHand.setAttribute("id","right")
+
+      rightHand.setAttribute("hand-controls")
+      leftHand.setAttribute("hand-controls")
+
+      leftHand.setAttribute("laser-controls","hand:left")
+      rightHand.setAttribute("laser-controls","hand: right")
+
+      leftHand.setAttribute("raycaster","showLine: true;lineColor: blue; lineOpacity: 1;objects: [animationcustom]")
+      rightHand.setAttribute("raycaster","showLine: true;lineColor: red; lineOpacity: 1;objects: [animationcustom]")
+
+      camera.append(leftHand)
+      camera.append(rightHand)
+    }
 }
